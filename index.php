ba6669462922b6cc1b946eb4ea3cc03b6670c862
zitj <1eyedea2eyedeas@gmail.com>
2020-06-15 17:20:48 +0200

    Improving UI/UX

--- index.php
+++ index.php
@@ -10,9 +10,9 @@
 <div class="container">
         <h3><?php echo $head_title?></h3>
     <form action="">
-        <label for="emaim">Your e-mail <span></span></label>
+        <label for="emaim">Your e-mail <span> &nbsp;</span></label>
         <input type="text">
-        <label for="password">Your password <span></span></label>
+        <label for="password">Your password <span>&nbsp;</span></label>
         <input type="password">
         
         <button>Sign in</button>
