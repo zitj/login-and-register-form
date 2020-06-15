fb459a77675800b8a890609bda74d778ba8f15c8
zitj <1eyedea2eyedeas@gmail.com>
2020-06-15 15:36:05 +0200

    first styling changes

--- index.php
+++ index.php
@@ -8,14 +8,18 @@
 <body>
 
 <div class="container">
+        <h3>Welcome to Galactica</h3>
     <form action="">
-        <label for=""></label>
+        <label for="emaim">Your e-mail</label>
         <input type="text">
-        <label for=""></label>
-        <input type="text">
+        <label for="password">Your password</label>
+        <input type="password">
         <button>Log in</button>
+        
     </form>
+    <a href="#">Register here</a>
+
 </div><!-- end .container -->
-    
+
 </body>
 </html>
