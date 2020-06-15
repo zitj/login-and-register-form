2834dc195d6d8a2d70a5ddf034e90dc7e03d1a04
zitj <1eyedea2eyedeas@gmail.com>
2020-06-15 14:34:48 +0200

    starting project

---
+++ index.php
@@ -0,0 +1,15 @@
+<?php 
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Log in form</title>
+</head>
+<body>
+    <h1><?php echo "We are connected!"?></h1>
+</body>
+</html>
