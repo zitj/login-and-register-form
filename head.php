a2beeb1e3f14d77d7f78dc50a0233c551557704d
zitj <1eyedea2eyedeas@gmail.com>
2020-06-15 16:30:01 +0200

    Improving UI

--- head.php
+++ head.php
@@ -6,5 +6,6 @@ $head_title;
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="css/style.css">
+    <link href="https://fonts.googleapis.com/css2?family=Open+Sans:wght@300;400;600&display=swap" rel="stylesheet">
     <title><?php echo $head_title ?></title>
 </head>
--- index.php
+++ index.php
@@ -10,12 +10,14 @@
 <div class="container">
         <h3>Welcome to Galactica</h3>
     <form action="">
-        <label for="emaim">Your e-mail</label>
+        <label for="emaim">Your e-mail <span></span></label>
         <input type="text">
-        <label for="password">Your password</label>
+        <label for="password">Your password <span></span></label>
         <input type="password">
-        <button>Log in</button>
         
+        <button>Log in</button>
+        <div class="borderRight"></div>
+        <div class="borderBottom"></div>
     </form>
     <a href="#">Register here</a>
 
