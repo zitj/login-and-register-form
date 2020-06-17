563d1309d1ab7c73a2a056ea6f407d2cd1ebd35b
zitj <1eyedea2eyedeas@gmail.com>
2020-06-17 20:40:49 +0200

    updated profile.php page

--- head.php
+++ head.php
@@ -5,7 +5,7 @@ $head_title;
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="cascade/style.css">
+    <link rel="stylesheet" href="css/style.css">
     <link href="https://fonts.googleapis.com/css2?family=Open+Sans:wght@300;400;600&display=swap" rel="stylesheet">
     <title><?php echo $head_title ?></title>
 </head>
